9c8a94a5c71d97865a684c839967d20a6ba23aed
ExperienceNote <qp4321234@gmail.com>
2018-03-26 21:16:24 +0530

    Added views and migrated tables

--- Head/app1/views.py
+++ Head/app1/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 def index(rquest):
-    return HttpResponse("<h1>Hey There!! </h1>")
+    return render(request,'app1/home.html')
